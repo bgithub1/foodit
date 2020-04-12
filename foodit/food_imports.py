5ee81908a3a6389b064a4053269c8e568c910a8b
henry perlman <bperlman1@gmail.com>
2020-04-12 13:38:34 -0400

    initial

---
+++ foodit/food_imports.py
@@ -0,0 +1,10 @@
+import sel_scrape as sc
+import pandas as pd
+import sys,os
+import datetime
+import urllib.request as ur
+import importlib
+import datetime
+import time
+import pdb
+import pathlib
